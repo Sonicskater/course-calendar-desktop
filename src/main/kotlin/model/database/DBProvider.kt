f01e149ffbe7b3065bd9eeea7f2e3dc19e93247b
Devon Hockley <devon@hockley.ca>
2019-03-14 15:03:13 -0600

    fixed gradle file to allow JUnit 4 Testing

--- src/main/kotlin/model/database/DBProvider.kt
+++ src/main/kotlin/model/database/DBProvider.kt
@@ -3,12 +3,13 @@ package model.database
 
 
 object DBProvider{
-    lateinit var strategy: EConnectionStrategies private set
+    var strategy: EConnectionStrategies = EConnectionStrategies.NONE
+        private set
 
     val connection: IDBConnection by lazy { openConnection()}
 
     fun init(strat : EConnectionStrategies = EConnectionStrategies.SQLite){
-        if (this::strategy.isInitialized){
+        if (this.strategy!=EConnectionStrategies.NONE){
             throw InitException(EInitExceptionCodes.ALREADY_INIT)
         }else{
             this.strategy = strat
@@ -16,7 +17,7 @@ object DBProvider{
     }
 
     private fun openConnection() : IDBConnection{
-        if (this::strategy.isInitialized) {
+        if (this.strategy!=EConnectionStrategies.NONE) {
             return when (strategy) {
                 EConnectionStrategies.SQLite -> SQLiteStrategy()
                 EConnectionStrategies.NONE -> throw InitException(EInitExceptionCodes.NOT_INIT)
